3da61e498949533223242a11adee0304caaf2b42
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-09-20 12:35:51 +0300

    Refactored assets

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
     <div class="site-header">
         <div class="container">
             <a href="{{ route('index') }}" class="branding">
-                <img src="storage/images/logo.png" alt="" class="logo">
+                <img src="{{ url('storage/images/logo.png') }}" alt="" class="logo">
                 <div class="logo-type">
                     <h1 class="site-title">Weather</h1>
                     <small class="site-description">Don't give up</small>
@@ -13,8 +13,6 @@
                 <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
                 <ul class="menu">
                     <li class="menu-item @yield('index-active')"><a href="{{ route('index') }}">Home</a></li>
-                    <li class="menu-item @yield('news-active')"><a href="{{ route('news') }}">News</a></li>
-                    <li class="menu-item @yield('photos-active')"><a href="{{ route('photos') }}">Photos</a></li>
                     <li class="menu-item @yield('contact-active')"><a href="{{ route('contact') }}">Contact</a></li>
                 </ul> <!-- .menu -->
             </div> <!-- .main-navigation -->
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,19 +8,12 @@
     <title> @yield('title') </title>
 
     <!-- Loading third party fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.min.css" integrity="sha512-PIAUVU8u1vAd0Sz1sS1bFE5F1YjGqm/scQJ+VIUJL9kNa8jtAWFUDMu5vynXPDprRRBqHrE8KKEsjA7z22J1FA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Loading main css file -->
-    <link rel="stylesheet" href="style.css">
-
-    <!--[if lt IE 9]>
-    <script src="js/ie-support/html5.js"></script>
-    <script src="js/ie-support/respond.js"></script>
-    <![endif]-->
-
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
 </head>
-
 
 <body>
 
@@ -34,9 +27,9 @@
 
 </div>
 
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/app.js"></script>
+<script src="{{ url('assets/js/jquery-1.11.1.min.js') }}"></script>
+<script src="{{ url('assets/js/min/plugins-min.js') }}"></script>
+<script src="{{ url('assets/js/min/app-min.js') }}"></script>
 
 </body>
 
