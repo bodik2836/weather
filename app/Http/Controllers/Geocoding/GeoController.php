7ee2d4a921a4b5b1f60053de2c13260b37bdf670
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-09-26 17:09:24 +0300

    Added weather method to GeoController

--- app/Http/Controllers/Geocoding/GeoController.php
+++ app/Http/Controllers/Geocoding/GeoController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Response;
 class GeoController extends Controller
 {
     protected string $geoUri = 'https://api.openweathermap.org/geo/1.0/direct';
+    protected string $weatherUri = 'https://api.openweathermap.org/data/2.5/weather';
 
     /**
      * @throws \GuzzleHttp\Exception\GuzzleException
@@ -33,5 +34,32 @@ class GeoController extends Controller
         ]);
 
         return response(json_decode($response->getBody()->getContents()), $response->getStatusCode());
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function weather(Request $request): Response
+    {
+        $data = $request->validate([
+            'lat' => 'required|string|max:255',
+            'lon' => 'required|string|max:255'
+        ]);
+
+        $client = new Client(['base_uri' => $this->weatherUri]);
+        $response = $client->request('GET', '', [
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+            'query' => [
+                'lat' => $data['lat'],
+                'lon' => $data['lon'],
+                'appid' => env('WEATHER_API_KEY'),
+                'units' => 'metric',
+            ]
+        ]);
+//        dd(json_decode($response->getBody()->getContents()));
+
+        return response($response->getBody()->getContents(), $response->getStatusCode());
     }
 }
