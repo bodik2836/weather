7ede8bbbe500dca93840890b22e492c20532fda2
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-04 17:03:03 +0300

    Added 5 days forecast method to weather service

--- app/Services/OpenWeatherMapApi/OpenWeatherMap.php
+++ app/Services/OpenWeatherMapApi/OpenWeatherMap.php
@@ -7,8 +7,8 @@ abstract class OpenWeatherMap
     const API_URIs = [
         'geocoding'       => 'https://api.openweathermap.org/geo/1.0/direct',
         'current_weather' => 'https://api.openweathermap.org/data/2.5/weather',
+        'forecast'        => 'https://api.openweathermap.org/data/2.5/forecast',
     ];
-
     private string $weatherApiKey = '';
 
     public function __construct()
--- app/Services/OpenWeatherMapApi/WeatherApi/WeatherService.php
+++ app/Services/OpenWeatherMapApi/WeatherApi/WeatherService.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Response;
 class WeatherService extends OpenWeatherMap
 {
     protected string $currentWeatherUri = self::API_URIs['current_weather'];
+    protected string $forecastUri = self::API_URIs['forecast'];
 
     /**
      * @throws \GuzzleHttp\Exception\GuzzleException
@@ -16,6 +17,27 @@ class WeatherService extends OpenWeatherMap
     public function currentWeather(array $data): Response
     {
         $client = new Client(['base_uri' => $this->currentWeatherUri]);
+        $response = $client->request('GET', '', [
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+            'query' => [
+                'lat' => $data['lat'],
+                'lon' => $data['lon'],
+                'appid' => $this->getWeatherApiKey(),
+                'units' => 'metric',
+            ]
+        ]);
+
+        return response($response->getBody()->getContents(), $response->getStatusCode());
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function forecast(array $data): Response
+    {
+        $client = new Client(['base_uri' => $this->forecastUri]);
         $response = $client->request('GET', '', [
             'headers' => [
                 'Accept' => 'application/json',
