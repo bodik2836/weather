5cb7f3f8275c48aa1a89911e41600aa75b1a3014
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-06 16:52:40 +0300

    Included notify js plugin

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.min.css" integrity="sha512-PIAUVU8u1vAd0Sz1sS1bFE5F1YjGqm/scQJ+VIUJL9kNa8jtAWFUDMu5vynXPDprRRBqHrE8KKEsjA7z22J1FA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Loading main css file -->
-    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body>
@@ -28,9 +28,10 @@
 
 </div>
 
-<script src="{{ url('assets/js/min/jquery-1.11.1.min.js') }}"></script>
-<script src="{{ url('assets/js/plugins.js') }}"></script>
-<script src="{{ url('assets/js/app.js') }}"></script>
+<script src="{{ asset('assets/js/min/jquery-1.11.1.min.js') }}"></script>
+<script src="{{ asset('assets/js/min/notify.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
 
 </body>
 
